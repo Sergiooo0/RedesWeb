4ca42cd73b2d9efe09e369dcbb02cc940b4a981d
Sergio <sergiogonzalezrodriguez05@gmail.com>
2023-12-16 12:27:32 +0100

    Base de datos en alwaysData

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 from flask import Flask, render_template, redirect, url_for, request, jsonify, session
-import sqlite3
+import mysql.connector
 
 app = Flask("Mi web")
 app.secret_key = 'super secret key'
-# Configuración de la base de datos (SQLite)
-DATABASE = 'usuarios.db'
+# Configuración de la base de datos (MySQL)
+DATABASE = {
+    'host': 'mysql-sergiogonzalez.alwaysdata.net',
+    'user': '340093_yo',
+    'password': 'Sergio07*',
+    'database': 'sergiogonzalez_yo'
+}
+
 
 def create_table():
-    with sqlite3.connect(DATABASE) as conn:
+    with mysql.connector.connect(**DATABASE) as conn:
         cursor = conn.cursor()
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS usuarios (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                username TEXT NOT NULL,
-                password TEXT NOT NULL
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                username VARCHAR(255) NOT NULL,
+                password VARCHAR(255) NOT NULL
             )
         ''')
+
 
 @app.route('/')
 def index():
@@ -35,39 +42,52 @@ def login():
     username = request.form.get('username')
     password = request.form.get('password')
 
-    with sqlite3.connect(DATABASE) as conn:
-        cursor = conn.cursor()
+    # Establecer la conexión a MySQL
+    conn = mysql.connector.connect(**DATABASE)
+    cursor = conn.cursor()
 
-        # Consultar si el usuario y la contraseña coinciden en la base de datos
-        cursor.execute("SELECT * FROM usuarios WHERE username=? AND password=?", (username, password))
-        user = cursor.fetchone()
+    # Consultar si el usuario y la contraseña coinciden en la base de datos
+    cursor.execute("SELECT * FROM usuarios WHERE username=%s AND password=%s", (username, password))
+    user = cursor.fetchone()
+
+    # Cerrar la conexión
+    cursor.close()
+    conn.close()
 
     if user:
-        session['usuario']=username
+        session['usuario'] = username
         return redirect(url_for('dashboard'))
     else:
         return "Credenciales incorrectas"
-
+    
 @app.route('/register', methods=['POST'])
 def register():
     username = request.form.get('username')
     password = request.form.get('password')
 
-    with sqlite3.connect(DATABASE) as conn:
-        cursor = conn.cursor()
+    # Establecer la conexión a MySQL
+    conn = mysql.connector.connect(**DATABASE)
+    cursor = conn.cursor()
 
-        # Verificar si el usuario ya existe en la base de datos
-        cursor.execute("SELECT * FROM usuarios WHERE username=?", (username,))
-        existing_user = cursor.fetchone()
+    # Verificar si el usuario ya existe en la base de datos
+    cursor.execute("SELECT * FROM usuarios WHERE username=%s", (username,))
+    existing_user = cursor.fetchone()
 
-        if existing_user:
-            return "El usuario ya está registrado"
-        
-        # Insertar el nuevo usuario en la base de datos
-        cursor.execute("INSERT INTO usuarios (username, password) VALUES (?, ?)", (username, password))
-        conn.commit()
+    if existing_user:
+        # Cerrar la conexión
+        cursor.close()
+        conn.close()
+        return "El usuario ya está registrado"
 
-    session['usuario']=username
+    # Insertar el nuevo usuario en la base de datos
+    cursor.execute("INSERT INTO usuarios (username, password) VALUES (%s, %s)", (username, password))
+    conn.commit()
+
+    # Cerrar la conexión
+    cursor.close()
+    conn.close()
+
+    session['usuario'] = username
     return redirect(url_for('dashboard'))
 
 @app.route('/dashboard')
